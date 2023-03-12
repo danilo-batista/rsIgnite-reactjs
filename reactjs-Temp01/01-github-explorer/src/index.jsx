b6960df597b8c7ff527b87680069ecef11176c6c
Danilo Batista <dbatistadsn@gmail.com>
2023-03-12 17:36:50 -0300

    fix: update 'render' method with new specifications from 'react.js 18'.

--- reactjs-Temp01/01-github-explorer/src/index.jsx
+++ reactjs-Temp01/01-github-explorer/src/index.jsx
@@ -1,5 +1,11 @@
-import { render } from 'react-dom';
+/* Na versão REACT 18, precisamos importar o 'createRoot' do pacote
+'react-dom/client. */
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-/* Render: recebe dois parâmetros: o que eu quero exibir e dentro de que elemento será exibido. */
-render(<App />, document.getElementById('root'));
+/* Crie um container com o elemento que vai receber o que será exibido. */
+const container = document.getElementById('root');
+/* Depois passe este container para oa função `createRoot`, colocando em uma variável. */
+const root = createRoot(container);
+/* Por último, utiliza o método 'render' da variável 'root'criada para inserir o elemento que quer exibir.*/
+root.render(<App />);
